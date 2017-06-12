58d1345bf131304e602aae1382a7bda23270cfba
diawest82 <diallowest@gmail.com>
2017-06-11 19:59:10 -0500

    Create wsgi.py

--- mineral_catalog/wsgi.py
+++ mineral_catalog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mineral_catelog project.
+WSGI config for mineral_catalog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mineral_catelog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mineral_catalog.settings")
 
 application = get_wsgi_application()

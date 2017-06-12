28e0dbd250742a806a67cf34d50cd1592178e913
diawest82 <diallowest@gmail.com>
2017-06-11 19:58:33 -0500

    Create urls.py

--- mineral_catalog/urls.py
+++ mineral_catalog/urls.py
@@ -1,4 +1,4 @@
-"""mineral_catelog URL Configuration
+"""mineral_catalog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
